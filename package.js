8647bcef3be811a9227a12f30136a4d220b1f8e3
Ben Lavalley <ben.lavalley@kaseya.com>
2019-10-29 01:25:54 -0400

    version bump

--- package.js
+++ package.js
@@ -1,27 +1,27 @@
 Package.describe({
-	name: "gadicohen:sitemaps",
-  summary: "functions to easily output valid sitemaps",
-  version: "0.0.26",
-  git: 'https://github.com/gadicc/meteor-sitemaps.git'
+	name: 'gadicohen:sitemaps',
+	summary: 'functions to easily output valid sitemaps',
+	version: '0.0.27',
+	git: 'https://github.com/gadicc/meteor-sitemaps.git',
 });
 
-Package.onUse(function(api) {
-	api.versionsFrom("METEOR@0.9.0");
-  api.use(['meteor', 'webapp', 'check', 'underscore'], 'server');
+Package.onUse(function (api) {
+	api.versionsFrom('METEOR@1.8.1');
+	api.use(['meteor', 'webapp', 'check', 'underscore'], 'server');
 
-  api.use('gadicohen:robots-txt@0.0.9', 'server');
+	api.use('gadicohen:robots-txt@0.0.11', 'server');
 
-  api.add_files('sitemaps.js', 'server');
-  api.export('sitemaps', 'server');
+	api.add_files('sitemaps.js', 'server');
+	api.export('sitemaps', 'server');
 });
 
 Npm.depends({
-	'xml2js': '0.4.8',
-	'zlibjs': '0.2.0',
+	xml2js: '0.4.22',
+	zlibjs: '0.3.1',
 });
 
-Package.onTest(function(api) {
-  api.use(['tinytest', 'http', 'random'], 'server');
-  api.use('gadicohen:sitemaps');
-  api.addFiles('tests.js', 'server');
+Package.onTest(function (api) {
+	api.use(['tinytest', 'http', 'random'], 'server');
+	api.use('gadicohen:sitemaps');
+	api.addFiles('tests.js', 'server');
 });
